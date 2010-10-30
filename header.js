405219b9256303759e412cfe9006ebc82c4f01c9
Sean Wilkinson <wilkinson@rice.edu>
2010-10-29 19:24:22 -0500

    First commit, plus a User Page experiment ...

---
+++ header.js
@@ -0,0 +1,28 @@
+//- JavaScript source code
+
+//- header.js ~~
+//  This defines the libraries, etc. needed to run the program in 'main.js'.
+//                                                          ~~ SRW, 29 Oct 2010
+
+if (this.window === undefined) {
+    throw "this demonstration has not been mplemented for headless contexts.";
+}
+
+var load = function () {
+        var args = Array.prototype.slice.call(arguments), i, s;
+        for (i = 0; i < args.length; i += 1) {
+            s = document.createElement('script');
+            s.src = args[i];
+            document.body.appendChild(s);
+        }
+    },
+
+    print = function () {
+        var args = Array.prototype.slice.call(arguments),
+            sink = document.getElementById('output-log');
+        for (i = 0; i < args.length; i += 1) {
+            sink.innerHTML += '<div>' + args[i] + '</div>';
+        }
+    };
+
+//- vim:set syntax=javascript:
---
+++ main.js
@@ -0,0 +1,13 @@
+//- JavaScript source code
+
+//- main.js ~~
+//  This is the driver for the entire project. It looks like a C program when I
+//  organize things this way, hahaha.
+//                                                          ~~ SRW, 29 Oct 2010
+
+function main() {
+    print("Hello world!");
+    return 0;
+}
+
+//- vim:set syntax=javascript:
